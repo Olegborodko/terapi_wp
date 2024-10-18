543e29b3767cd85d5eaeeca37fb6087d43bc6ded
borodkoleg <borodkoleg@gmail.com>
2024-10-18 23:34:29 +0300

    footer done

--- themes/consultum/footer.php
+++ themes/consultum/footer.php
@@ -14,22 +14,29 @@
 
 <footer>
   <div class="footer-block custom-containder">
-    <div class="logo-block">
-      <?php $logo_url = get_template_directory_uri() . '/assets/img/footer-logo.svg'; ?>
-      <a class="logo ms-auto" href="#"><img src="<?= esc_url($logo_url) ?>" alt="footer logo"></a>
+    <div class="description">
+      <div class="text">
+        אתר אינטרנט הוא פלטפורמה דיגיטלית אתר אינטרנט הוא פלטפורמה דיגיטלית המתמקדת במתן גישה נוחהאתר אינטרנט הוא פלטפורמה דיגיטלית
+      </div>
+      <div class="social">
+        <?php $logo_fb = get_template_directory_uri() . '/assets/img/fs.svg'; ?>
+        <?php $logo_inst = get_template_directory_uri() . '/assets/img/inst.svg'; ?>
+        <?php $logo_med = get_template_directory_uri() . '/assets/img/med.svg'; ?>
+        <a class="" href="#"><img src="<?= esc_url($logo_med) ?>" alt="logo med"></a>
+        <a class="" href="#"><img src="<?= esc_url($logo_inst) ?>" alt="logo instagram"></a>
+        <a class="" href="#"><img src="<?= esc_url($logo_fb) ?>" alt="logo facebook"></a>
+      </div>
     </div>
 
-    <ul>
+    <ul class="footer-menu">
       <li>בַּיִת</li>
       <li>מטפל</li>
       <li>בלוג</li>
     </ul>
 
-    <div class="description">
-      <div class="text">
-        אתר אינטרנט הוא פלטפורמה דיגיטלית אתר אינטרנט הוא פלטפורמה דיגיטלית המתמקדת במתן גישה נוחהאתר אינטרנט הוא פלטפורמה דיגיטלית
-      </div>
-      
+    <div class="logo-block">
+      <?php $logo_url = get_template_directory_uri() . '/assets/img/footer-logo.svg'; ?>
+      <a class="logo ms-auto" href="#"><img src="<?= esc_url($logo_url) ?>" alt="footer logo"></a>
     </div>
   </div>
 </footer>
--- themes/consultum/header.php
+++ themes/consultum/header.php
@@ -27,8 +27,8 @@
   <div id="page" class="site general_bg_color">
     <header class="custom-containder" dir="ltr">
       <nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Переключить навигацию">
+        <div class="container-fluid p-0">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="">
             <span class="navbar-toggler-icon"></span>
           </button>
 
