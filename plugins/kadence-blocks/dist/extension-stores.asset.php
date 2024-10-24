16f9802bd7d71196cf3766fbd87244e3d7a69901
borodkoleg <borodkoleg@gmail.com>
2024-10-24 15:59:05 +0300

    add plugin Enable Accessibility

--- plugins/kadence-blocks/dist/extension-stores.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('lodash', 'wp-data'), 'version' => '0a07c02eb54b54d1445b');
--- plugins/kadence-blocks/dist/icons.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react'), 'version' => '1a6e6d1e66822fb24557');
